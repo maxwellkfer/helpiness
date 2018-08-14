e59f4259b8945f1bdae8444d931b68d08c3b4b93
Lucinda Redfern <lucindaredfern@gmail.com>
2018-08-14 11:55:04 +0100

    progress so far

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,40 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.service = Service.find[:service_id]
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:status)
   end
 end
