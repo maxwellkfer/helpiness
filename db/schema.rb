272c4b08883874d73b3a7df2f339092817762651
Maxime GUERIN <maxwellkfer@gmail.com>
2018-08-16 14:39:39 +0100

    sign up ok

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2018_08_16_111115) do
 
   create_table "universities", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "web_page"
     t.string "country"
     t.string "domain"
