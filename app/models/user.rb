a0647f4cde13d7ec1d199e3728f268052d182aef
Maxime GUERIN <maxwellkfer@gmail.com>
2018-08-20 15:48:38 +0100

    changes dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ before_save :default_values
     if avatar_url
       avatar_url
     else
-      "background.jpg"
+      "https://m.media-amazon.com/images/M/MV5BMjI5MzgxMTQ4M15BMl5BanBnXkFtZTgwNjczMTk0NzE@._V1_UX477_CR0,0,477,268_AL_.jpg"
     end
   end
 
