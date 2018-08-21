2b94e4ed6bcf7b0bd527fa87a688e3f68d344644
Maxime GUERIN <maxwellkfer@gmail.com>
2018-08-21 09:21:36 +0100

    reviews deleted

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  resources :services do
-    resources :reviews
-  end
+  resources :services
 end
