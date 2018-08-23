dccf63c00c6a29948db804b2d9e26e6059331255
Maxime GUERIN <maxwellkfer@gmail.com>
2018-08-23 15:23:56 +0100

    foreign key

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 class Service < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   has_many :customers, through: :bookings
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@
   # :confirmable, :lockable, :timeoutable and :omniauthable
   mount_uploader :avatar, AvatarUploader
   after_create :send_welcome_email
-  has_many :skills
-  has_many :services
-  has_many :bookings
-  has_many :reviews
+  has_many :skills, dependent: :destroy
+  has_many :services, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   belongs_to :university, optional: true
 
